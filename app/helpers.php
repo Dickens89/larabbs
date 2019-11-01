8ada445c653d817da730b0df2148144a56576b62
yizhou_homestead <15120073241@126.com>
2019-11-01 11:14:27 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -2,11 +2,17 @@
 /**
  * Created by PhpStorm.
  * User: yizhou
- * Date: 2019-10-18
- * Time: 19:33
+ * Date: 2019-11-01
+ * Time: 11:09
  */
 
- function route_class()
- {
-     return str_replace('.', '-', Route::currentRouteName());
- }
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
+
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
